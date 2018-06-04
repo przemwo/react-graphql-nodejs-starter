6269c04fe3c7cf35b7c1a8a5000d19ac6681d080
przemwo <przemwo@o2.pl>
2018-06-04 22:50:46 +0200

    added Apollo to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import './App.css';
 import PostList from './components/PostList';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1 className="header">Welcome to React-GraphQL-NodeJS starter project!</h1>
-        <PostList />
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          <h1 className="header">Welcome to React-GraphQL-NodeJS starter project!</h1>
+          <PostList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
