e84e3d34e02793e2112f42343d26ee8b2ae4b22f
przemwo <przemwo@o2.pl>
2018-06-04 22:34:17 +0200

    added PostList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import PostList from './components/PostList';
 
 class App extends Component {
   render() {
     return (
-      <h1 className="header">Welcome to React-GraphQL-NodeJS starter project!</h1>
+      <div>
+        <h1 className="header">Welcome to React-GraphQL-NodeJS starter project!</h1>
+        <PostList />
+      </div>
     );
   }
 }
---
+++ client/src/components/PostList/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render() {
+        return(
+            <ul>
+                <li>post</li>
+            </ul>
+        );
+    }
+}
+export default PostList;
